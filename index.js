60e527169a0a06091ff864990b8db6928ae54bdd
zdurr22 <zdurr22@gmail.com>
2021-10-04 17:44:52 -0400

    commit tree 8dcb7bbe01d9b439734dd82721e9eac886e55319

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 62;
+const num2 = 1;
+let multiply =  num1 * num2;
+
+let random = Math.ceil(Math.random())
+
+const num3 = 12;
+const num4 =  8;
+let mod = num3 % num4
+
+let max = Math.max(1,2,3,4,5,20)
